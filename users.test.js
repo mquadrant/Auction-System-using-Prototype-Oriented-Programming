728d88cdd96faaa3460f75dfb6f751bb280e5da0
Benjamin Mark <mquadrant@gmail.com>
2019-04-26 16:18:52 +0100

    Added the test case to test second users creation

--- users.test.js
+++ users.test.js
@@ -1,9 +1,14 @@
 const Users = require('./users');
 
-describe('Creating User through the constructor',()=>{
+describe('Creating two Users through the constructor',()=>{
 
-    it('should return a user object',()=>{
+    it('should return a user object for the first user',()=>{
         let charity = new Users('Charity','charity45@gmail.com','dhhS34*7');
         expect(charity).toEqual({id: 1, name:'Charity',email:'charity45@gmail.com',password:'dhhS34*7'});
+    });
+
+    it('should return a user object for the second user',()=>{
+        let charity = new Users('Tommy','ctommy@gmail.com','d3434*7');
+        expect(charity).toEqual({id: 2, name:'Tommy',email:'ctommy@gmail.com',password:'d3434*'});
     });
 });
