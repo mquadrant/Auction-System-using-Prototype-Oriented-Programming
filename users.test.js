2818dca99242aa74a4fc8536ecb5e219b90968f1
Benjamin Mark <mquadrant@gmail.com>
2019-05-06 09:19:39 +0100

    Added a test case to create one auction and add to db

--- users.test.js
+++ users.test.js
@@ -1,6 +1,7 @@
 const Users = require('./users');
+const Auction = require('./auctions');
 
-describe('Creating two Users through the constructor',()=>{
+describe('Creating two Users through the Users constructor',()=>{
 
     it('should return a user object for the first user',()=>{
         let charity = new Users('Charity','charity45@gmail.com','dhhS34*7');
@@ -11,4 +12,14 @@ describe('Creating two Users through the constructor',()=>{
         let charity = new Users('Tommy','ctommy@gmail.com','d3434*7');
         expect(charity).toEqual({id: 2, name:'Tommy',email:'ctommy@gmail.com',password:'d3434*7'});
     });
+});
+
+describe('Creating two Auctions through the Auction constructor',()=>{
+
+    it('should return an auction object for the first auction',()=>{
+        let auctionOne = new Auction('Golden shoe lace','Shoe lace used by Bill Gate',100000,1);
+        expect(auctionOne).toEqual({id: 1, productName:'Golden shoe lace',productDescription:'Shoe lace used by Bill Gate',
+        minimumBidAmount:100000,userId:1});
+    });
+
 });
