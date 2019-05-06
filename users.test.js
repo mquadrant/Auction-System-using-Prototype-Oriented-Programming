88fc5c05557c7a0ce14eb04ef0445429f271ebb5
Benjamin Mark <mquadrant@gmail.com>
2019-05-06 12:24:38 +0100

    Removed two test cases of auction constructor

--- users.test.js
+++ users.test.js
@@ -15,33 +15,29 @@ describe('Creating two Users through the Users constructor', () => {
     });
 });
 
-describe('Creating two Auctions through the Auction constructor', () => {
-
-    it('should return an auction object for the first auction', () => {
-        let auctionOne = new Auction('Golden shoe lace', 'Shoe lace used by Bill Gate', 100000, 1);
-        expect(auctionOne).toEqual({
-            id: 1, productName: 'Golden shoe lace', productDescription: 'Shoe lace used by Bill Gate',
-            minimumBidAmount: 100000, userId: 1
-        });
-    });
-    it('should return an auction object for the first auction', () => {
-        let auctionTwo = new Auction('Silver wrist watch', 'Ancient Silver watch of all time', 303000, 2);
-        expect(auctionTwo).toEqual({
-            id: 2, productName: 'Silver wrist watch', productDescription: 'Ancient Silver watch of all time',
-            minimumBidAmount: 303000, userId: 2
-        });
-    });
-
-});
 
 describe('Creating Auctions through the Users prototype', () => {
 
     it('should return an auction object for the first auction', () => {
-        let auctionOne = tommy.createAuction('Limousine 2019 Model', 'Latest Limo in town', 5000000,2);
-        expect(auctionOne).toEqual({
-            id: 3, productName: 'Limousine 2019 Model', productDescription: 'Latest Limo in town',
+        let createAuction = tommy.createAuction('Limousine 2019 Model', 'Latest Limo in town', 5000000,2);
+        expect(createAuction).toEqual({
+            id: 1, productName: 'Limousine 2019 Model', productDescription: 'Latest Limo in town',
             minimumBidAmount: 5000000, userId: 2
         });
+    });
+
+    
+
+});
+
+describe('User should be able to view all auctions', () => {
+
+    it('should all available auctions', () => {
+        let allAuctions = tommy.viewAllAuctions();
+        expect(allAuctions).toEqual([{
+            id: 1, productName: 'Limousine 2019 Model', productDescription: 'Latest Limo in town',
+            minimumBidAmount: 5000000, userId: 2
+        }]);
     });
 
 });
