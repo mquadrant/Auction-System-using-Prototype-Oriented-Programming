175b374437ae5e479efd5088e7a092fe10f76830
Benjamin Mark <mquadrant@gmail.com>
2019-05-06 12:37:10 +0100

    added test case to test viewAllAuctions and bug fixed in auctions.js

--- auctions.js
+++ auctions.js
@@ -7,9 +7,8 @@ this.minimumBidAmount = minimumBidAmount;
 this.userId = userId;
 
 //Saving product for auction into the auction db
-
 let length = db.auctions.length;
-let id = length === 0 ? 1 : ++db.auctions[length - 1].id;
+let id = length === 0 ? 1 : db.auctions[length - 1].id+1;
 this.id = id;
 db.auctions.push({
     id,
--- users.test.js
+++ users.test.js
@@ -16,17 +16,23 @@ describe('Creating two Users through the Users constructor', () => {
 });
 
 
-describe('Creating Auctions through the Users prototype', () => {
+describe('Creating two Auctions through the Users prototype', () => {
 
     it('should return an auction object for the first auction', () => {
-        let createAuction = tommy.createAuction('Limousine 2019 Model', 'Latest Limo in town', 5000000,2);
+        let createAuction = tommy.createAuction('Limousine 2019 Model', 'Latest Limo in town', 5000000);
         expect(createAuction).toEqual({
             id: 1, productName: 'Limousine 2019 Model', productDescription: 'Latest Limo in town',
             minimumBidAmount: 5000000, userId: 2
         });
     });
 
-    
+    it('should return an auction object for the second auction', () => {
+        let createAuction = tommy.createAuction('Silver Wrist Watch', 'Ancient wrist watch', 700000);
+        expect(createAuction).toEqual({
+            id: 2, productName: 'Silver Wrist Watch', productDescription: 'Ancient wrist watch',
+            minimumBidAmount: 700000, userId: 2
+        });
+    });
 
 });
 
@@ -34,10 +40,14 @@ describe('User should be able to view all auctions', () => {
 
     it('should all available auctions', () => {
         let allAuctions = tommy.viewAllAuctions();
+        console.log(allAuctions);
         expect(allAuctions).toEqual([{
             id: 1, productName: 'Limousine 2019 Model', productDescription: 'Latest Limo in town',
             minimumBidAmount: 5000000, userId: 2
-        }]);
+        },
+        {
+            id: 2, productName: 'Silver Wrist Watch', productDescription: 'Ancient wrist watch',
+            minimumBidAmount: 700000, userId: 2}]);
     });
 
 });
